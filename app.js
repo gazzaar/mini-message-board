78208cbb5613a2e6c27e740fb1afd5f5e72ac00a
gazzaar <gazzarDev@gmail.com>
2025-03-05 16:14:16 +0200

    styles: Style the app

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { router: indexRouter } = require('./routes/index.js');
 const path = require('node:path');
-const newMessageRouter = require('./routes/new.js');
 const app = express();
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+const newMessageRouter = require('./routes/new.js');
 app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
