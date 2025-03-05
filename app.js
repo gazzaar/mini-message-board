916b68d5f0302c1f6aec14cdbd89c24e9096948f
gazzaar <gazzarDev@gmail.com>
2025-03-05 02:31:14 +0200

    use new message route

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const indexRouter = require('./routes/index.js');
+const { router: indexRouter } = require('./routes/index.js');
 const path = require('node:path');
+const newMessageRouter = require('./routes/new.js');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/', indexRouter).listen(3000);
+app.use('/', indexRouter);
+app.use('/new', newMessageRouter);
+app.listen(3000);
